d752ef63eb2949ae8410be2505d7fe3b373ff4a6
Shivam Gupta <37578595+gilann@users.noreply.github.com>
2019-01-13 01:52:13 +0530

    Create sumby2_1.js

---
+++ sumby2_1.js
@@ -0,0 +1,14 @@
+function sumByTwo(array, target_sum){
+    var i, j;
+    for(i=0; i<array.length; i++){
+        for(j=i+1; j<array.length; j++){
+            if(array[i]+array[j]==target_sum){
+                //found, return
+                return [array[i], array[j]];
+            }
+        }
+    }
+    return "No such pair found";
+}
+var result = sumByTwo([4,5,-12,-2,8,9], -14);
+console.log(result);
